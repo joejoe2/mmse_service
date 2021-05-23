ac962995eb383505cbdc7eb3c5e4cd8edcf6a373
joejoe2 <39475537+joejoe2@users.noreply.github.com>
2021-05-23 10:46:40 +0800

    change action question

--- survey/MMSE/questions/action_question.py
+++ survey/MMSE/questions/action_question.py
@@ -20,8 +20,8 @@ def three_action_question():
     question["question id"] = "mmse-action-three_action"
     question["category"] = "action"
     question["type"] = "three_action"
-    question["info"] = "ask user to do three continuous action, action type is in data"
-    question["data"] = {"type": "moving a cube to three positions"}
+    question["info"] = "ask user to do continuous action, action type is in data"
+    question["data"] = {"type": "walk"}
     # question["user answer"] = {}
     # question["user score"] = 0
     question["full score"] = 3
